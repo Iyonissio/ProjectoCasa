5d233b957ecca0c3936339d9c918fe9ef7f16617
Iyonissio Daniel Sitoe <57901563+Iyonissio@users.noreply.github.com>
2021-11-24 14:41:42 +0200

    contact app created

--- backend/contacts/views.py
+++ backend/contacts/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework.views import APIView
+from .models import Contact
+from django.core.mail import message, send_mail
+from rest_framework.response import Response
 
-# Create your views here.
+class ContactCreateView(APIView):
+    permissions_classes = (permissions.AllowAny, )
+
+    def post(self, request, format=None):
+        data = self.request.data
+
+        try:
+            send_mail(
+                data['subject'],
+                'Name: '
+                + data['name']
+                + '\nEmail: '
+                + data['email']
+                + '\n\nMessage:\n'
+                + data['message'],
+                'mbryoyo01@gmail.com',
+                ['mbryoyo01@gmail.com'],
+                fail_silently=False
+            )
+
+            contact = Contact(name=data['name'], email=data['email'], subject=data['subject'], message=data['message'])
+            contact.save()
+
+            return Response({'success': 'Messagem enviada com sucesso'})
+
+        except:
+            return Response({'error': 'Messagem nao enviada , Falha'})
--- backend/realest_estate/settings.py
+++ backend/realest_estate/settings.py
@@ -88,6 +88,12 @@ DATABASES = {
     }
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'mbryoyo01@gmail.com'
+EMAIL_HOST_PASSWORD = 'mmgcsohyghxuoeaw'
+EMAIL_USE_TLS = True
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
